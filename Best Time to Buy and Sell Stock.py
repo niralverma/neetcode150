3e5d123f3c6b85cacfd62b888f965a200156c36f
Niral Verma <76837066+niralverma@users.noreply.github.com>
2023-03-18 17:22:29 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        left,right=0,1
+        max_profit=0
+        while right<len(prices):
+            if prices[left]<prices[right]:
+                profit=prices[right]-prices[left]
+                max_profit=max(max_profit,profit)
+            
+            else:
+                left=right
+            
+            right+=1
+        
+        return max_profit
+        
